3ac6bf06b9c30b3ea9ee40e5824c29324e389e8c
Aymie Chalouhy <aymie.walid.chalouhy@gmail.com>
2021-05-23 22:37:47 +0300

    step 7

---
+++ basic-01/Step_7.php
@@ -0,0 +1,13 @@
+<?php
+$birth_year= 1996;
+$current_year= 2021;
+
+$age = $current_year - $birth_year;
+
+if ($age > 18)
+echo "You can drive";
+
+else
+echo "You still a kid, go and play GTA"
+
+?>
