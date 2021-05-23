90e6b4b51befd04f63c56fe0c549b3e029f6aa94
Aymie Chalouhy <aymie.walid.chalouhy@gmail.com>
2021-05-23 23:21:48 +0300

    step 8

---
+++ basic-01/Step_8.php
@@ -0,0 +1,21 @@
+
+ <?php
+ $items = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+ echo "Hello sir, do you have " . $items[0] . ", " . $items[1] . ", " . $items[2] . "? <br/>
+ Also if you sell fruits can I find a " .$items[5];
+
+echo "</br></br>";
+
+$itemmm = array (
+    "eggs" => array("Balade","mazere3"),
+    "milk" => array("Fresh", "Taanayel"),
+    "waterpack" => array("Tanoureen","Reem")
+  );
+  echo "Hey Sir, Please I need 1 pack of " . $itemmm["eggs"][0]. " eggs <br/> and  3 " 
+  .$itemmm["waterpack"][1]. " Water Pack" ;
+
+ 
+ ?> 
+
+
+
