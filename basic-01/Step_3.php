fe31ce9b68a3751ae3e15434ecd69e01faea7b4e
Aymie Chalouhy <aymie.walid.chalouhy@gmail.com>
2021-05-23 18:19:03 +0300

    step 3

---
+++ basic-01/Step_3.php
@@ -0,0 +1,12 @@
+
+<?php
+$name ="Aymie";
+$email="aymie.walid.chalouhy@gmail.com";
+echo "Hello </br> My name is: $name </br> &  My email is: $email <br /> ";
+
+$height= 180;
+$width= 60;
+
+$area= $height*$width;
+echo "<br /> The area value is: $height * $width= $area"
+?>
